dfd6b1223ea14c1419f5b6d89b93e01c0f5b2e38
Yuldoshova <ozoda8171@gmail.com>
2024-11-05 18:16:33 +0500

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('MOVIE')
+    .setDescription('The movie app apis')
+    .setVersion('1.0')
+    .addTag('apis')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   const config = app.get(ConfigService)
 
-  await app.listen(config.get<number>('appConfig.port'), config.get<string>('appConfig.host'), () => {
-    console.log(`Server running port on ${config.get<number>('appConfig.port')}`)
-  });
+  await app.listen(
+    config.get<number>('appConfig.port'),
+    config.get<string>('appConfig.host'),
+    () => console.log(`Server running port on ${config.get<number>('appConfig.port')}`)
+  );
 }
 bootstrap();
