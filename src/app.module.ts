7746f769258580808b90a9350fc2a8f203ce4bb8
Yuldoshova <ozoda8171@gmail.com>
2024-11-05 18:04:27 +0500

    added config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { appConfig } from './config/app.config';
+import { dbConfig } from './config/db.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+    load:[appConfig, dbConfig]
+  })]
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService)
+
+  await app.listen(config.get<number>('appConfig.port'), config.get<string>('appConfig.host'), () => {
+    console.log(`Server running port on ${config.get<number>('appConfig.port')}`)
+  });
 }
 bootstrap();
